9c8a18c5d2a9d529e1ef124246bcc029eae305b2
Amol <amolpukale23@gmail.com>
2019-11-15 15:23:53 +0530

    newewst

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express=require("express");
+var express=require(express);
 var app=express();
-
+var parser=require(body-parser);
 app.get("/",function(request,response){
     response.send(" In get function");
 })
-
+app.use(body-parser.json);
 app.listen("4000",function(req,res){
     res.send("Hi");
 })
